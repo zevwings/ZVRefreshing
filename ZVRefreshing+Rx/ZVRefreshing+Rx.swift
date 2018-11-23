fd30a9f483954a277c952eef93302cc1e482ca90
zevwings <zev.wings@gmail.com>
2018-11-23 21:46:27 +0800

    add rx support

---
+++ ZVRefreshing+Rx/ZVRefreshing+Rx.swift
@@ -0,0 +1,34 @@
+//
+//  ZVRefreshing+Rx.swift
+//  ADJAddress
+//
+//  Created by 张伟 on 2018/11/23.
+//  Copyright © 2018 zevwings. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+import ZVRefreshing
+
+extension Reactive where Base: ZVRefreshComponent {
+    
+    /// Reactive wrapper for `ValueChanged` control event.
+    /// Call it when `isRefreshing` == true
+    public var refresh: Observable<Bool> {
+        return controlEvent(.valueChanged)
+            .map { self.base.isRefreshing }
+            .filter { $0 == true }
+    }
+    
+    /// Bindable sink for `beginRefreshing()`, `endRefreshing()` methods.
+    public var isRefreshing: Binder<Bool> {
+        return Binder(self.base) { refreshControl, refresh in
+            if refresh {
+                refreshControl.beginRefreshing()
+            } else {
+                refreshControl.endRefreshing()
+            }
+        }
+    }
+
+}
--- ZVRefreshing.podspec
+++ ZVRefreshing.podspec
@@ -20,11 +20,20 @@ Pod::Spec.new do |s|
   s.author       = { "zevwings" => "zev.wings@gmail.com" }
   s.platform     = :ios, "8.0"
   s.source       = { :git => "https://github.com/zevwings/ZVRefreshing.git", :tag => "#{s.version}" }
-  s.source_files = "ZVRefreshing/**/*.swift", "ZVRefreshing/ZVRefreshing.h"
   s.resources    = "ZVRefreshing/Resource/*"
-
   s.requires_arc = true
+  s.default_subspec = 'Core'
 
-  s.dependency 'ZVActivityIndicatorView'
+  s.subspec 'Core' do |core|
+  	core.source_files = "ZVRefreshing/**/*.swift", "ZVRefreshing/ZVRefreshing.h"
+    core.dependency "ZVActivityIndicatorView"
+  end
+
+  s.subspec 'RxSwift' do |rx|
+  	rx.source_files  = "ZVRefreshing+Rx/**/*.swift", "ZVRefreshing+Rx/ZVRefreshing-Rx.h"
+  	rx.dependency = "ZVRefreshing/Core"
+  	rx.dependency = "RxSwift"
+  	rx.dependency = "RxCocoa"
+  end
 
 end
